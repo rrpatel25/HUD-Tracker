1e3723a69d09c576b8dbeb1d84d288a65b878248
Ravi <i.rrpatel@gmail.com>
2017-11-07 18:12:22 -0600

    add user authentication measures and validation method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,19 @@ class User < ApplicationRecord
   validates :username, :password_digest, presence: true
   validates :username, uniqueness: true
   validate :password_length
+
+  def password
+    @password ||= BCrypt::Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @new_password = new_password
+    @password = BCrypt::Password.create(new_password)
+    self.password_digest = @password
+  end
+
+  def password_length
+    password_error_text = 'must be at least 7 characters long'
+    errors.add(:password, password_error_text) unless @new_password.length >= 7
+  end
 end
