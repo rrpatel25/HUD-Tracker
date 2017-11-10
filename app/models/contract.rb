467cc4884f4b09347b0c8faacb7c8bfbb32cf335
Ravi <i.rrpatel@gmail.com>
2017-11-09 18:31:24 -0600

    add Contract model and relationships with Card and Level

---
+++ app/models/contract.rb
@@ -0,0 +1,4 @@
+class Contract < ApplicationRecord
+  has_one :card, as: :item
+  belongs_to :level, foreign_key: :level_code
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110000125) do
+ActiveRecord::Schema.define(version: 20171110002555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 20171110000125) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_collections_on_code", unique: true
+  end
+
+  create_table "contracts", force: :cascade do |t|
+    t.string "level_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "levels", force: :cascade do |t|
