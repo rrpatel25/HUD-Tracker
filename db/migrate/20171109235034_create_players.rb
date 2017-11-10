4e4f8632eea483388345573bd25fcbdd275133d9
Ravi <i.rrpatel@gmail.com>
2017-11-10 15:35:05 -0600

    edit buy_price and sell_price from string to integer data type; edit overall_rating field from string to integer data_type

--- db/migrate/20171109235034_create_players.rb
+++ db/migrate/20171109235034_create_players.rb
@@ -4,7 +4,7 @@ class CreatePlayers < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :collection_code
       t.string :level_code
-      t.string :overall_rating
+      t.integer :overall_rating
 
       t.timestamps
     end
