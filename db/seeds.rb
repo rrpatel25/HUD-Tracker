e18bdedfd0522f56b8efb979c514f27379bdc057
Dan Dewar <dan.dewar10@gmail.com>
2017-11-10 00:13:28 -0600

    Adding seeds for collections
    These seeds will give us categories to group player cards into. These are only for players, collectables will have their own collection type. These seeds are just a start, we will have to add the new colelctions manually to keep from wiping the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,94 @@
 Level.create!(code: "bronze", description: "Bronze")
 Level.create!(code: "silver", description: "Silver")
 Level.create!(code: "gold", description: "Gold")
+
+collections = [
+  {
+    code: 'base',
+    description: 'Base'
+  },
+  {
+    code: 'ROOK',
+    description: 'Rookies'
+  },
+  {
+    code: 'MSP',
+    description: 'Master Set'
+  },
+  {
+    code: 'HERO',
+    description: 'Hero'
+  },
+  {
+    code: 'Gold',
+    description: 'Base'
+  },
+  {
+    code: 'DC',
+    description: 'Draft Champions'
+  },
+  {
+    code: 'HDB',
+    description: '???'
+  },
+  {
+    code: 'CC',
+    description: 'Centenial'
+  },
+  {
+    code: 'LGD',
+    description: 'Legend'
+  },
+  {
+    code: 'TOTW',
+    description: 'Team of the Week'
+  },
+  {
+    code: 'VE',
+    description: 'Vegas Expansion'
+  },
+  {
+    code: 'EVO',
+    description: 'Evolution'
+  },
+  {
+    code: 'MS',
+    description: 'Milestone'
+  },
+  {
+    code: 'PT',
+    description: 'Prime Time'
+  },
+  {
+    code: 'FB',
+    description: 'Flashback'
+  },
+  {
+    code: 'FF',
+    description: 'Flash Forward'
+  },
+  {
+    code: 'HFC',
+    description: 'Hockey Fights Cancer'
+  },
+  {
+    code: 'FO',
+    description: 'Face Off'
+  },
+  {
+    code: 'HWN',
+    description: 'Halloween'
+  },
+  {
+    code: 'MOV',
+    description: 'Movember'
+  },
+  {
+    code: 'SOTM',
+    description: 'Stars of the Month'
+  }
+]
+
+collections.each do |code, description|
+  Collection.create!(code: code, description: description)
+end
