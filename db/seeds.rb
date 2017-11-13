8668876b1ee89880b12e0601d51219f552ad5b28
Dan Dewar <dan.dewar10@gmail.com>
2017-11-12 21:19:33 -0600

    More Seeds Added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -94,9 +94,85 @@ collections = [
   }
 ]
 
-collections.each do |code, description|
-  Collection.create!(code: code, description: description)
+collections.each do |collection|
+  Collection.create!(code: collection[:code], description: collection[:description])
 end
 
-Synergy.create!(code: "LL", description: "Locker Room Leader")
-Synergy.create!(code: "S", description: "Silky Smooth")
+synergies = [
+  {
+    code: 'NZ',
+    description: 'Neutral Zone Defender'
+  },
+  {
+    code: 'P',
+    description: 'Protect the Puck'
+  },
+  {
+    code: 'RS',
+    description: 'Rocket Skates'
+  },
+  {
+    code: 'GD',
+    description: 'Puck Drops Gloves Drop'
+  },
+  {
+    code: 'HS',
+    description: 'Human Shield'
+  },
+  {
+    code: 'DZ',
+    description: 'Dangler Zone'
+  },
+  {
+    code: 'PM',
+    description: 'Pinball Machine'
+  },
+  {
+    code: 'FS',
+    description: 'Fine Shooting'
+  },
+  {
+    code: 'BM',
+    description: 'Breakout Master'
+  },
+  {
+    code: 'PP',
+    description: 'Percision Passing'
+  },
+  {
+    code: 'HM',
+    description: 'Hammer Time'
+  },
+  {
+    code: 'CP',
+    description: 'Clutch Player'
+  },
+  {
+    code: 'FF',
+    description: 'Frequent Fighter'
+  },
+  {
+    code: 'LL',
+    description: 'Locker Room Leaders'
+  },
+  {
+    code: 'SS',
+    description: 'Silky Smooth'
+  },
+  {
+    code: 'TN',
+    description: 'Thread the Needle'
+  },
+  {
+    code: 'AA',
+    description: 'Angry Aggressors'
+  },
+  {
+    code: 'CW',
+    description: 'Concrete Wall'
+  }
+]
+
+synergies.each do |synergy|
+  Synergy.create!(code: synergy[:code], description: synergy[:description])
+end
